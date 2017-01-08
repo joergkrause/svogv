01d9599e62818f37e17383105f3be1e818257b82
Joerg Krause <joerg@krause.net>
2017-01-08 20:44:59 +0100

    Make tabs interactive to routing

--- src/lib/widgets/ac-tabs.ts
+++ src/lib/widgets/ac-tabs.ts
@@ -1,4 +1,6 @@
 ﻿import { Component, Input, Output, OnChanges, EventEmitter } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd, Params, PRIMARY_OUTLET } from "@angular/router";
+
 
 /**
  * Create a single tab for tab views. The link shall be a regular route array.
@@ -59,13 +61,64 @@ export class AcTabs {
   @Input() tabs: AcTabData;
   @Output() currentTab: AcTab;
 
-  constructor() {
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {
   }
 
   activateTab(tab: AcTab) {
     this.currentTab = tab;
     this.tabs.tabs.forEach(t => t.active = false);
-    tab.active = true; 
+    tab.active = true;
+  }
+
+  ngOnInit() {
+    // put data: { "breadcrumb": true, "subtitle": "Sub Route Name" } in the router config for those items that shall appear in the breadcrumb 
+    const ROUTE_DATA_BREADCRUMB: string = "breadcrumb";
+    const ROUTE_DATA_SUBTITLE: string = "subtitle";
+
+    //subscribe to the NavigationEnd event
+    this.router.events.filter(event => event instanceof NavigationEnd).subscribe(event => {
+      //reset breadcrumbs
+
+      //get the root route
+      let currentRoute: ActivatedRoute = this.activatedRoute.root;
+
+      //set the url to an empty string
+      let url: string = "";
+
+      //iterate from activated route to children
+      if (currentRoute.children.length > 0) {
+        let childrenRoutes: ActivatedRoute[] = currentRoute.children;
+
+        //iterate over each children
+        childrenRoutes.forEach(route => {
+          //set currentRoute to this route
+          currentRoute = route;
+
+          //verify this is the primary route
+          if (route.outlet !== PRIMARY_OUTLET) {
+            return;
+          }
+
+          //verify the custom data property "breadcrumb" is specified on the route
+          if (!route.snapshot.data.hasOwnProperty(ROUTE_DATA_BREADCRUMB)) {
+            return;
+          }
+
+          //get the route's URL segment
+          let routeURL: string = route.snapshot.url.map(segment => segment.path).join("/");
+
+          //append route URL to URL
+          url += `/${routeURL}`;
+
+          //add router data as current tab
+          var matchTab = this.tabs.tabs.filter(t => t.link.toString() == url || ((<any>t).length > 0 && t.link[0] == url));
+          if (matchTab && matchTab.length == 1) {
+            this.currentTab = matchTab[0];
+          }
+
+        });
+      }
+    });
   }
 
 }
