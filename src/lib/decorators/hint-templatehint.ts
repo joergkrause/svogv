d0a573c135ef71d294cdeacecd10eba1297bf1c2
Joerg Krause <joerg@krause.net>
2017-02-02 20:39:00 +0100

    Implemented autoform

--- src/lib/decorators/hint-templatehint.ts
+++ src/lib/decorators/hint-templatehint.ts
@@ -9,9 +9,9 @@
  * @param template        The Name that appears in form fields as a watermark.
  * @param params          Depending of template some additional values as a dictionary.
  */
-export function TemplateHint(template: string, params: [{ key: string, value: any }]) {
+export function TemplateHint(template: string, params: { key: string, value: any }[]) {
     // the original decorator
-    function templateHintInternal(target: Object, name: string, template: string, params: [{ key: string, value: any }]): void {
+    function templateHintInternal(target: Object, name: string, template: string, params: { key: string, value: any }[]): void {
         new templateHintInternalSetup(target, name, template, params);
     }
 
@@ -21,7 +21,7 @@ export function TemplateHint(template: string, params: [{ key: string, value: an
 
 class templateHintInternalSetup {
 
-    constructor(public target: any, public key: string, public template: string, public params: [{ key: string, value: any }]) {
+    constructor(public target: any, public key: string, public template: string, public params: { key: string, value: any }[]) {
 
         // create a helper property to transport a meta data value
         Object.defineProperty(this.target, `__templateHint__${this.key}`, {
--- src/lib/widgets/ac-editors/ac-editor.ts
+++ src/lib/widgets/ac-editors/ac-editor.ts
@@ -11,16 +11,19 @@ import { FormGroup } from '@angular/forms';
                   [ngClass]="{ 'has-danger': !userForm.controls[name].valid && userForm.controls[name].touched }">
               <label [attr.for]="name" [attr.title]="tooltip" class="col-form-label col-xs-2">{{ label }}: </label>
               <div [ngClass]="{ 'col-xs-10': inline }">
-                <textarea *ngIf="type == 'textarea'" class="form-control" [id]="name" [formControlName]="name">
+                <textarea *ngIf="type == 'textarea'" class="form-control" [id]="name" 
+                          [formControlName]="name" [attr.rows]="getParams('rows')" [attr.cols]="getParams('cols')">
                 </textarea>
                 <select *ngIf="type == 'select-enum'" class="form-control" [id]="name" [formControlName]="name">
                   <option *ngFor="let option of enumValues" [value]="option.key">{{option.val}}</option>
                 </select>
-                <input *ngIf="type == 'range'" [placeholder]="waterMark" type="range" [attr.minvalue]="fromValue" [attr.maxvalue]="toValue" class="form-control" [id]="name" [formControlName]="name" />
-                <input *ngIf="type == 'calendar'" [placeholder]="waterMark" type="date" class="form-control" [id]="name" [formControlName]="name" />
-                <input *ngIf="type == 'number'" [placeholder]="waterMark" type="number" class="form-control" [id]="name" [formControlName]="name" />
+                <input *ngIf="type == 'range'" [placeholder]="waterMark" type="range" [attr.minvalue]="fromValue" 
+                       [attr.maxvalue]="toValue" class="form-control" [id]="name" [formControlName]="name" [title]="tooltip" />
+                <input *ngIf="type == 'calendar'" [placeholder]="waterMark" type="date" class="form-control" [id]="name" [formControlName]="name" [title]="tooltip" />
+                <input *ngIf="type == 'number'" [placeholder]="waterMark" type="number" class="form-control" [id]="name" [formControlName]="name" [title]="tooltip" />
+                <input *ngIf="type == 'boolean'" type="checkbox" class="form-control" [id]="name" [formControlName]="name" [title]="tooltip" />
                 <input *ngIf="type == 'text' || type == ''" [placeholder]="waterMark" type="text" class="form-control" [id]="name" [formControlName]="name" />
-                <input *ngIf="type == 'hidden' [id]="name" [formControlName]="name" type="hidden" />
+                <input *ngIf="type == 'hidden'" [id]="name" [formControlName]="name" type="hidden" />
                 <span class="fa fa-warning text-danger form-control-feedback" 
                       [hidden]="userForm.controls[name].valid || userForm.controls[name].pristine"></span>
                 <small class="text-danger" 
@@ -44,11 +47,28 @@ export class AcEditor implements OnInit {
   @Input() fromValue: number = 0;
   @Input() toValue: number = 100;
   @Input() waterMark = "";
+  // additional values provided by TemplateHint decorator 
+  params: { key: string, value: any }[];
 
   errors: Array<string>;
 
 
   constructor() {
+    this.params = new Array<{ key: string, value: any }>();
+  }
+
+  getParams(key: string): any {
+    return this.first(this.params.filter((e) => e.key === key), 0);
+  }
+
+  first(array: Array<{ key: string, value: any }>, n: number): any {
+    if (array == null)
+      return void 0;
+    if (n == null)
+      return array[0];
+    if (n < 0)
+      return [];
+    return array.slice(0, n);
   }
 
   ngOnInit() {
@@ -57,6 +77,19 @@ export class AcEditor implements OnInit {
     var editorModel = (<any>this.userForm)['__editorModel__'];
     // get type from form
     if (editorModel) {
+      // get elementary types, this might get overwritten later according to decorators found
+      if (typeof editorModel[this.name] === "string") {
+        this.type = "text";
+      }
+      if (editorModel[this.name] instanceof Boolean) {
+        this.type = "boolean";
+      }
+      if (editorModel[this.name] instanceof Number) {
+        this.type = "number";
+      }
+      if (editorModel[this.name] instanceof Date){
+        this.type = "calendar";
+      }
       // make an instance to read the properties
       this.label = editorModel[`__displayName__${this.name}`] || this.label || this.name;
       this.tooltip = editorModel[`__displayDesc__${this.name}`] || this.tooltip || this.name;
@@ -73,7 +106,14 @@ export class AcEditor implements OnInit {
       if (editorModel[`__hasWatermark__${this.name}`]) {
         this.waterMark = editorModel[`__watermark__${this.name}`];
       }
-      
+      // templates
+      if (editorModel[`__hasTemplateHint__${this.name}`]) {
+        this.type = (<string>editorModel[`__templateHint__${this.name}`]).toLowerCase();
+        if (editorModel[`__templateHintParams__${this.name}`]) {
+          this.params = (<{ key: string, value: any }[]>editorModel[`__templateHintParams__${this.name}`]);
+        }
+      }
+
       // render hidden fields as hidden even in forms
       if (editorModel[`__isHidden__${this.name}`]) {
         this.type = "hidden";
