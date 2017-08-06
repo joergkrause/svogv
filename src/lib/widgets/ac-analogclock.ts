b0db58d4537c13a3b20d599d729db3a6b219588b
Joerg Krause <joerg@krause.net>
2017-08-06 19:11:37 +0200

    clock improvements

--- src/lib/widgets/ac-analogclock.ts
+++ src/lib/widgets/ac-analogclock.ts
@@ -1,7 +1,9 @@
-﻿import { Component, Inject, 
-         Input, Output,
-         EventEmitter, 
-         AfterViewInit, ViewChild, ElementRef, Renderer } from '@angular/core';
+﻿import {
+    Component, Inject,
+    Input, Output,
+    EventEmitter,
+    AfterViewInit, OnChanges, ViewChild, ElementRef, Renderer
+} from '@angular/core';
 import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
@@ -10,35 +12,57 @@ import { DOCUMENT } from '@angular/platform-browser';
               display: block;
             }`],
     template: `
-<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="200" height="200" #svg>
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1" 
+     [attr.width]="diameter * 2" 
+     [attr.height]="diameter * 2" #svg>
     <filter id="innerShadow" x="-20%" y="-20%" width="140%" height="140%">
         <feGaussianBlur in="SourceGraphic" stdDeviation="3" result="blur"/>
         <feOffset in="blur" dx="2.5" dy="2.5"/>
     </filter>
     <g>
-        <circle id="shadow" style="fill:rgba(0,0,0,0.1)" cx="97" cy="100" r="87" 
+        <circle id="shadow" style="fill:rgba(0,0,0,0.1)" 
+                [attr.cx]="diameter - 3" 
+                [attr.cy]="diameter" 
+                [attr.r]="diameter - 3" 
                 filter="url(#innerShadow)"></circle>
-        <circle id="circle" style="stroke: #FFF; stroke-width: 12px;" cx="100" 
-                [style.fill]="bgColor" cy="100" r="80"></circle>
+        <circle id="circle" style="stroke: #FFF; stroke-width: 12px;" 
+                [style.stroke-width]="diameter * 0.12"
+                [attr.cx]="diameter" 
+                [attr.cy]="diameter"
+                [attr.fill]="bgColor" 
+                [attr.r]="diameter - (diameter * 0.2)"></circle>
     </g>
     <g>
-        <line x1="100" y1="100" x2="100" y2="55" transform="rotate(90 100 100)" 
-              style="stroke-width: 4px; stroke: #fffbf9;" #hourhandTransform>
+        <line [attr.x1]="diameter" [attr.y1]="diameter" 
+              [attr.x2]="diameter" [attr.y2]="diameter * 0.55" transform="rotate(90 100 100)" 
+              style="stroke-width: 4px; stroke: #fffbf9;" #hourhandTransform
+              [style.stroke]="hourColor"
+              >
         </line>
-        <line x1="100" y1="100" x2="100" y2="40" transform="rotate(180 100 100)" 
-              style="stroke-width: 3px; stroke: #fdfdfd;" #minutehandTransform>
+        <line [attr.x1]="diameter" [attr.y1]="diameter" 
+              [attr.x2]="diameter" [attr.y2]="diameter * 0.40" transform="rotate(180 100 100)" 
+              style="stroke-width: 3px; stroke: #fdfdfd;" #minutehandTransform
+              [style.stroke]="minuteColor"
+              >
         </line>
-        <line x1="100" y1="100" x2="100" y2="30" transform="rotate(270 100 100)" 
-              style="stroke-width: 2px; stroke: #C1EFED;" #secondhandTransform >
+        <line [attr.x1]="diameter" [attr.y1]="diameter" 
+              [attr.x2]="diameter" [attr.y2]="diameter * 0.30" transform="rotate(270 100 100)" 
+              style="stroke-width: 2px; stroke: #C1EFED;" #secondhandTransform 
+              [style.stroke]="secondColor"
+              >
         </line>
     </g>
-    <circle id="center" style="fill:#128A86; stroke: #C1EFED; stroke-width: 2px;" cx="100" cy="100" r="3"></circle>
+    <circle id="center" style="fill:#128A86; stroke: #C1EFED; stroke-width: 2px;" 
+            [attr.cx]="diameter" [attr.cy]="diameter" r="3"></circle>
 </svg>     
 `
 })
-export class AcAnalogClock implements AfterViewInit {
+export class AcAnalogClock implements AfterViewInit, OnChanges {
 
     @Input() bgColor: string;
+    @Input() secondColor = '#C1EFED';
+    @Input() minuteColor = '#fdfdfd';
+    @Input() hourColor = '#fffbf9';
     @Input() diameter: number;
     @Input() showSeconds: boolean;
     @Output() minuteClock: EventEmitter<Date>;
@@ -52,25 +76,19 @@ export class AcAnalogClock implements AfterViewInit {
         this.minuteClock = new EventEmitter<Date>();
         this.hourClock = new EventEmitter<Date>();
         this.bgColor = '#92345';
+        this.diameter = 100;
+    }
+
+    ngOnChanges(changes: any) {
+        if (changes['diameter']) {
+            this.makeMarks();
+        }
     }
 
     ngAfterViewInit() {
-        // make hour marks
-        for (var i = 1; i <= 12; i++) {
-            var el = this.document.createElementNS('http://www.w3.org/2000/svg', 'line');
-            el.setAttribute('x1', '100');
-            el.setAttribute('y1', '30');
-            el.setAttribute('x2', '100');
-            el.setAttribute('y2', '40');
-            el.setAttribute('transform', 'rotate(' + (i * 360 / 12) + ' 100 100)');
-            el.setAttribute('style', 'stroke: #ffffff;');
-            this.svg.nativeElement.appendChild(el);
-        }
-        // base area
-        let cx = 100;
-        let cy = 100;
+        this.makeMarks();
         // create animation string
-        let shifter = (val:number) => [val, cx, cy].join(' ');
+        let shifter = (val: number) => [val, this.diameter, this.diameter].join(' ');
         // // from real time
         let date = new Date();
         var secAngle: number = 360 * date.getSeconds() / 60;
@@ -91,8 +109,30 @@ export class AcAnalogClock implements AfterViewInit {
         }, 1000);
     }
 
-    setRotate(element: ElementRef, value: string): void{
+    setRotate(element: ElementRef, value: string): void {
         element.nativeElement.setAttribute('transform', `rotate(${value})`);
+    }
+
+    private makeMarks() {
+        let marks = this.svg.nativeElement.querySelectorAll('line[mark]');
+        if (marks && marks.length > 0) {
+            for (let i = 0; i < marks.length; i++) {
+                marks[i].remove();
+            }
+        }
+        // make hour marks
+        for (var i = 1; i <= 12; i++) {
+            var el = this.document.createElementNS('http://www.w3.org/2000/svg', 'line');
+            el.setAttribute('x1', this.diameter.toString());
+            el.setAttribute('y1', (this.diameter * 0.3).toString());
+            el.setAttribute('x2', this.diameter.toString());
+            el.setAttribute('y2', (this.diameter * 0.4).toString());
+            el.setAttribute('transform', 'rotate(' + (i * 360 / 12) + ` ${this.diameter} ${this.diameter})`);
+            el.setAttribute('style', 'stroke: #ffffff;');
+            el.setAttribute('mark', '');
+            this.svg.nativeElement.appendChild(el);
+        }
+
     }
 
 }
